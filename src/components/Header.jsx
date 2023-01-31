7a5e65cd7ce11c24e43f7d32e0e38ba962cbf14e
LeandroOnline <105742869+LeandroOnline@users.noreply.github.com>
2023-01-31 20:43:25 -0300

    feat:fixHero

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,11 @@
 import styled from "styled-components";
+import elipse from "../assets/Ellipse 1.png";
+import worker from "../assets/unsplash_KHO_jvns5Xc.png";
 
 const Div = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  padding: 0px;
   gap: 15.94px;
-  position: absolute;
   width: 350px;
   height: 300px;
-  top: 122px;
 `;
 
 const DivC = styled.div`
@@ -18,9 +13,7 @@ const DivC = styled.div`
   flex-direction: row;
   align-items: center;
   justify-content: flex-start;
-  /* padding: 6.64114px 6.64114px 6.64114px 42.5033px; */
   gap: 10.63px;
-  /* width: 399.13px; */
   width: 90%;
   height: 94.97px;
 `;
@@ -46,25 +39,74 @@ const DivGradient = styled.div`
   background: linear-gradient(#e3e70fc4, #ff06b882);
 `;
 
+const DivRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+
+  @media (max-width: 767px){
+    justify-content: center;
+  }
+`;
+
+const Img = styled.img`
+  width: 100%;
+  height: 100%;
+  align-self: end;
+`;
+
+const Img2 = styled.img`
+  width: 100%;
+  height: 100%;
+  align-self: end;
+  top: 25px;
+  left: 30px;
+  z-index: 2;
+  position: absolute;
+`;
+
+const Div2 = styled.div`
+  position: relative;
+  align-self: end;
+
+  @media (max-width: 900px) {
+    display: none;
+  }
+`;
+
 const Header = () => {
   return (
-    <Div>
-      <P fontweight="700">Innovation</P>
-      <DivC>
-        <DivGradient />
-        <P fontweight="400" fontSize="42.5033px">
-          Constructure
+    <DivRow>
+      <Div>
+        <P fontweight="700">Innovation</P>
+        <DivC>
+          <DivGradient />
+          <P fontweight="400" fontSize="42.5033px">
+            Constructure
+          </P>
+        </DivC>
+        <P
+          fontweight="400"
+          fontSize="9.29759px"
+          lineHeight="11px"
+          height="44px"
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+          purus sit amet luctus venenatis, lectus magna fringilla urna,
+          porttitor rhoncus dolor purus non enim praesent. Lorem ipsum dolor sit
+          amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus
+          venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus
+          non enim praesent
         </P>
-      </DivC>
-      <P fontweight="400" fontSize="9.29759px" lineHeight="11px" height="44px">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
-        purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor
-        rhoncus dolor purus non enim praesent. Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis,
-        lectus magna fringilla urna, porttitor rhoncus dolor purus non enim
-        praesent
-      </P>
-    </Div>
+      </Div>
+      <Div2>
+        <Img src={elipse} />
+        <Img2 src={worker} />
+      </Div2>
+    </DivRow>
   );
 };
 export default Header;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,23 +2,47 @@ import Header from "./Header";
 import Navbar from "./Navbar";
 import styled from "styled-components";
 import rectanglecinco from "../assets/Rectanglecinco.png";
+import fondo from "../assets/fondo.jpg";
 
 const Div = styled.div`
   background-image: linear-gradient(rgba(6, 5, 57, 0.83), rgba(6, 5, 57, 0.83)),
     url(${rectanglecinco});
   background-size: cover;
-  height: 600px;
+  height: 100vh;
   width: 100vw;
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding: 25px;
+
+  @media (min-width: 767px) {
+    background-image: linear-gradient(
+        rgba(6, 5, 57, 0.83),
+        rgba(6, 5, 57, 0.83)
+      ),
+      url(${fondo});
+  }
+`;
+
+const DivHeader = styled.div`
+  display: flex;
+  max-width: 1000px;
+  height: 100%;
+  align-items: center;
+  justify-content: start;
+
+  @media (max-width: 767px) {
+    justify-content: center;
+  }
 `;
 
 const Hero = () => {
   return (
     <Div>
       <Navbar />
-      <Header id='home'/>
+      <DivHeader>
+        <Header id="home" />
+      </DivHeader>
     </Div>
   );
 };
